e291083234389cdec737f9a767848582ec92cadb
Michael Lehn <michael.lehn@uni-ulm.de>
2022-05-28 11:23:39 +0200

    Better documentation

--- ulmas1/encode.h
+++ ulmas1/encode.h
@@ -7,8 +7,11 @@
 
 #include "cg.h"
 
+// each expr in vl will be destructed, i.e. freeExpr() is called
 uint32_t encodeInstr(enum CgSeg cgSeg, uint64_t addr, uint32_t opCode,
 		     va_list vl);
+
+// will destruct expr, i.e. freeExpr() is called
 uint64_t encodeExpr(enum CgSeg cgSeg, uint64_t addr, size_t numBytes,
 		    struct Expr *expr);
 
